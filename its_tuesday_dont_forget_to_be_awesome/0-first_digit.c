c3ea6e4e125a7cbf8a1e9a26e093a824316593b5
Zee Adams <wabluekey@gmail.com>
2016-02-24 17:36:00 -0500

    completed task 0, printing first digit

---
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -0,0 +1,22 @@
+#include "my_functions.h"
+#include <limits.h>
+
+/* this function prints the first digit of a number. */
+int first_digit(int n) {
+ 
+  if (n == INT_MIN) {  /* this statement accommodates the special case of the min integer. */
+      n = INT_MIN + 1;
+    }
+  if (n == 0) { 
+      return(n);
+    }
+  if (n < 0) { /* for negative numbers, we want to make them positive by multiplying them by -1. */
+      n = n * -1;
+    }
+  if (n > 0) { /* for all other possible numbers > 0, they are put through a while loop to get the first digit. */ 
+    while (n > 9){
+       n = (n / 10);
+       }
+    } 
+    return(n); 
+}
