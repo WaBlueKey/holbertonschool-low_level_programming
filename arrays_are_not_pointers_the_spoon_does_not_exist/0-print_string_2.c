8d192a40a5fec2bcefc2b330ea5d18b635408283
Zee Adams <wabluekey@gmail.com>
2016-03-12 00:29:50 -0500

    updated comments

--- arrays_are_not_pointers_the_spoon_does_not_exist/0-print_string_2.c
+++ arrays_are_not_pointers_the_spoon_does_not_exist/0-print_string_2.c
@@ -1,9 +1,9 @@
 int print_char(char c);
 
 void print_string_2(char *str) {
- 
+  /* function to print 1 char out of every 2 starting with 1st one.*/
   for (; *str >= '\0';) {
     print_char(*str);
-    str+=2;
+    str+=2; /* move two spots to the next character. */
   }
 }
--- arrays_are_not_pointers_the_spoon_does_not_exist/1-print_string_half.c
+++ arrays_are_not_pointers_the_spoon_does_not_exist/1-print_string_half.c
@@ -1,10 +1,12 @@
 int print_char(char c);
 
 void print_string_half(char *str) {
-  int l;
-  int p;
+  int l; 
+  int p; 
+  /* for the length of the string.*/
   for (l = 0; *(str + l); l++) {
   }
+  /* for printing the string.*/
   for (p = l/2; p < l; p++) {
     print_char(str[p+1]);
   }
