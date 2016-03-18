ccdcd961bd1b38a0a54847a9becbfdbbb17fbfc0
Zee Adams <wabluekey@gmail.com>
2016-03-17 22:40:57 -0400

    completed task 2.

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/1-nconcat_strings.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/1-nconcat_strings.c
@@ -1,18 +1,15 @@
 int print_char(char c);
-
+/* this function concatenates two strings. */
 char *nconcat_strings(char *dest, const char *src, int n) {
 
   int s;
   int d;
 
-  for (d = 0; dest[d] != '\0'; d++) {
+  for (d = 0; dest[d] != '\0'; d++) { 
   }
-  for (s = 0; src[s] != '\0'; s++) {
-    dest[d] = src[s];
-    d++;
+  for (s = 0; s < n && src[s] != '\0'; s++, d++) {
+  dest[d] = src[s];
   }
-  while (n > s) {
-    n = s;
-  }
+  dest[d+s] = '\0';
   return dest;
-  }
+}
