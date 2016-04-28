c6f0ed6902481480a6aa0f6fe9ebf5a68910bc90
Zee Adams <WaBlueKey@users.noreply.github.com>
2016-04-28 14:48:43 -0400

    fixed and commented code

--- cisdoublefun_day_2_pointers/3-rev_string.c
+++ cisdoublefun_day_2_pointers/3-rev_string.c
@@ -1,30 +1,23 @@
+/* this function reverses a string. */
 #include "my_functions.h"
 
 char *rev_string(char *str) {
-
-
-  int i = 0;
-  int j;
+  int length = 0; /* var to count the length of the string. */
+  int fcount = 0; /* var to keep front half count. */
+  int bcount = 0; /* var to keep back half count in swap. */
   char sub;
-  char *s;
-  s = &sub;
 
-  while(*(str + i) != '\0') {
-    i++;
+  while(*(str + length) != '\0') { /* stopping at null terminator. */
+    length ++;
   }
-
-  j = i;
-
-  while(j > 0) {
-    char temp;
-    temp = *(s + j);
-    *(str + j) =  *(str + i);
-    *(str + i) = temp;
-    i++;
-    j--;
-
+  fcount = length; /* setting forward count to length of string. */
+  while(bcount < length/2){ /* for loop, back count is used to start at 0 to
+                            half of length to divide up the string for swap.*/
+    sub = *(str + bcount); /* assign first character in the string to sub.*/
+    *(str + bcount) =  *(str + (fcount - 1)); /* begin swap first to last. */
+    *(str + (fcount - 1)) = sub; /* move sub value to the last string val. */
+    fcount --; /* reduce fcount by 1 for each loop iteration. */
+    bcount ++; /* increase bcount by 1 for each loop iteration. */
   }
-
-
-  return s;
+  return str;
 }
