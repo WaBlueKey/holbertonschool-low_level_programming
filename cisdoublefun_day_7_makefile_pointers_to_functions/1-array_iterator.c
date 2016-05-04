0e593865acc4834ed0ca7cb628f960731fb01961
Zee Adams <WaBlueKey@users.noreply.github.com>
2016-05-04 10:52:49 -0400

    Completed task 1

---
+++ cisdoublefun_day_7_makefile_pointers_to_functions/1-array_iterator.c
@@ -0,0 +1,13 @@
+/* this function executes another function given as a parameter on each
+element of an array. */
+
+void array_iterator(int *array, int size, void (*action_func)(int)) {
+
+  int i = 0;
+
+  while(i < size) {
+    action_func(array[i]);
+    i++;
+  }
+
+}
