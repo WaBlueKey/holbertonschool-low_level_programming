38e34dd9279dd66f63f257a2249d2d8a22d86b14
Zee Adams <WaBlueKey@users.noreply.github.com>
2016-05-09 18:53:08 -0400

    updated 0-print_content.c

--- file_descriptors/0-print_content/0-print_content.c
+++ file_descriptors/0-print_content/0-print_content.c
@@ -10,3 +10,27 @@
  *
  *  by Zee Adams, May 2016
  */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int ac, char *av[]) {
+
+  int r;
+  int fd;
+  char tmp[BUFSIZ];
+
+  if(ac == 1) {
+    return 1;
+  }
+  fd = open(av[1], O_RDONLY);
+  r = read(fd, tmp, BUFSIZ);
+
+  if(r > 0) {
+    write(1, tmp, r);
+  }
+
+  close(fd);
+  return(0);
+}
