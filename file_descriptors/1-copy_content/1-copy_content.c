ab76471a0e9420ec0d98a77d21f3fc8b89ae2451
Zee Adams <WaBlueKey@users.noreply.github.com>
2016-05-09 21:53:32 -0400

    updated 1-copy_content.c

--- file_descriptors/1-copy_content/1-copy_content.c
+++ file_descriptors/1-copy_content/1-copy_content.c
@@ -10,3 +10,31 @@
  *
  *  by Zee Adams, May 2016
  */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#define buf_size 512
+
+int main(int ac, char *av[]) {
+
+ int r;
+ int fd;
+ char tmp[BUFSIZ];
+ int c;
+
+
+ if(ac == 1 || ac > 3) { /* argument count outside of project parameters. */
+   return(1);
+ }
+ fd = open(av[1], O_RDONLY); 
+ r = read(fd, tmp, BUFSIZ);
+ close(fd);
+
+ c = open(av[2], O_RDWR|O_CREAT, 0644);
+   write(c, tmp, r);
+
+
+ close(c);
+ return(0);
+}
