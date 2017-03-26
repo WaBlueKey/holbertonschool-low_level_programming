0dc3598683c1e51a3e5d2d18d887c9a7ec31e9b4
Zee <wabluekey@gmail.com>
2017-03-26 19:21:40 +0000

    update to fix red checks

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -17,7 +17,6 @@ size_t print_listint(const listint_t *h)
 
 	size_t count = 0;
 
-  /* Loop to iterate over list to print and count nodes. */
 	while (h != NULL)
 	{
 
@@ -25,6 +24,6 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		count++;
 	}
-	/* Return number of nodes in list. */
+
 	return (count);
 }
