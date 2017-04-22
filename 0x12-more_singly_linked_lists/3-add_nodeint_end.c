03f69411540eab08b0f54a2412259a5781fde117
Zee <wabluekey@gmail.com>
2017-04-22 07:12:49 +0000

    update spacing betty check

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,29 +11,29 @@
  * Description: function that adds an elements to the beginning of listint_t
  * list - Task two of More singly linked lists project
  */
- listint_t *add_nodeint_end(listint_t **head, const int n)
- {
- 	listint_t *current = *head;
- 	listint_t *new_node = malloc(sizeof(listint_t));
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *current = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
 
- 	new_node->n = n;
- 	new_node->next = NULL;
- 	if (new_node == NULL)
- 	{
- 		return (NULL);
- 	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
- 	if (*head == NULL)
- 	{
- 		*head = new_node;
- 	}
- 	else
- 	{
- 		while (current->next != NULL)
- 		{
- 			current = current->next;
- 		}
- 		current->next = new_node;
- 	}
- 	return (new_node);
- }
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	return (new_node);
+}
